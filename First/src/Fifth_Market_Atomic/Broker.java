ae0182cc0095d956189601f66e5d3cbdb0da50c3
Nick <nickwtwfu@gmail.com>
2017-04-05 19:49:49 +0300

    added AtomicLong.MARKET.Broker.

---
+++ First/src/Fifth_Market_Atomic/Broker.java
@@ -0,0 +1,19 @@
+package Fifth_Market_Atomic;
+
+public class Broker extends Thread {
+    private Market market;
+    private static final int PAUSE = 500;
+    public Broker(Market market) {
+        this.market = market;
+    }
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("Current index: " + market.getIndex());
+                Thread.sleep(PAUSE);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
